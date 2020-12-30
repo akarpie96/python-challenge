9277a41d5657081581710f56c15a098a98fad499
Aaron Karpie <aaronkarpie@gmail.com>
2020-12-30 12:02:52 -0800

    second attempt

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,18 @@
 import os 
 import csv
 
-print("Hi")
+pybank_csv=os.path.join("..", "..", "PyBank", "Resources", "budget_data.csv")
+
+total_months=[]
+
+
+with open (pybank_csv, 'r') as csvfile :
+    csvreader=csv.reader(csvfile) 
+
+    for row in csvreader:
+        total_months.append(row[0])
+
+    
+print("Financial Analysis")
+print("-----------------------")
+print(f"Total Months: {len(total_months)}")
