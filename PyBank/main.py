d292452b2ccf51d6560de4502d836a7352c42e66
Aaron Karpie <aaronkarpie@gmail.com>
2020-12-30 14:24:28 -0800

    appending the total profit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,16 @@
 import os 
 import csv
 
-pybank_csv=os.path.join("..", "..", "PyBank", "Resources", "budget_data.csv")
+pybank_csv=os.path.join("..","PyBank", "Resources", "budget_data.csv")
 
 total_months=[]
-
+total_profit = []
 
 with open (pybank_csv, 'r') as csvfile :
     csvreader=csv.reader(csvfile) 
 
     for row in csvreader:
         total_months.append(row[0])
+        total_profit.append(row[1])
 
-    
-print("Financial Analysis")
-print("-----------------------")
-print(f"Total Months: {len(total_months)}")
+print(total_profit)
