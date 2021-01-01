7cccd6d8348d911aa606ca648168e806b66fea34
Aaron Karpie <aaronkarpie@gmail.com>
2021-01-01 09:35:19 -0800

    5th commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,9 @@ pybank_csv=os.path.join("..","PyBank", "Resources", "budget_data.csv")
 total_months=[]
 total_profit = []
 
+difference = []
+i=0 
+
 with open (pybank_csv, 'r') as csvfile :
     csvreader=csv.reader(csvfile) 
     csvheader=next(csvreader)
@@ -13,6 +16,15 @@ with open (pybank_csv, 'r') as csvfile :
     for row in csvreader:
         total_months.append(row[0])
         total_profit.append(row[1])
+
+
+while i<85: 
+    diff= int(total_profit[i+1]) - (int(total_profit[i]))
+
+    difference.append(diff)
+
+    i = i + 1
+         
 
 
 def sum(total_profit): 
@@ -28,10 +40,9 @@ def average(total_profit):
     for number in total_profit: 
         total += float(number)
     return total/length
-
- 
+print(difference)
 print("Financial Analysis")  
 print("-----------------------") 
 print(f"Total Months: {len(total_months)}")
 print(f"Total: ${round(sum(total_profit))}")
-print(f"Average Change : ${round((int(total_profit[85])-int(total_profit[0]))/((85)))}")
+print(f"Average Change : ${round((int(total_profit[85])-int(total_profit[0]))/((85)),2)}")
